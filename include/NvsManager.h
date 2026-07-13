966877dc4c730b51153aebbc9ec826a56027c6e6
Emilia Kura <emillia.kura@gmail.com>
2026-07-13 10:48:05 +0200

    feat: add NvsManager class
    - With configuration keys.
    - It will be responsible for storing data in nvs such as wifi credentials & device secret.

---
+++ include/NvsManager.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <Arduino.h>
+#include<Preferences.h>
+
+class NvsManager {
+    static Preferences prefs;
+    
+    static const char* NAMESPACE;
+    static const char* KEY_SSID;
+    static const char* KEY_PASS;
+    static const char* KEY_SECRET;
+    static const char* KEY_ACTIVATED;
+public:
+    NvsManager();
+
+};
---
+++ src/NvsManager.cpp
@@ -0,0 +1,7 @@
+#include "NvsManager.h"
+
+NvsManager::NvsManager()
+{
+}
+
+
