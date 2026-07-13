080f47647d3af2b5aff4bdb9e542b24d915e64ba
Emilia Kura <emillia.kura@gmail.com>
2026-07-13 10:50:35 +0200

    feat: add constants definitions

--- src/NvsManager.cpp
+++ src/NvsManager.cpp
@@ -1,5 +1,12 @@
 #include "NvsManager.h"
 
+Preferences NvsManager::prefs;
+const char* NvsManager::NAMESPACE = "device_cfg";
+const char* NvsManager::KEY_SSID = "wifi_ssid";
+const char* NvsManager::KEY_PASS = "wifi_pass";
+const char* NvsManager::KEY_SECRET = "dev_secret";
+const char* NvsManager::KEY_ACTIVATED = "activated";
+
 NvsManager::NvsManager()
 {
 }
