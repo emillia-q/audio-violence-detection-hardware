693d68bce1f130d5b7a9c95d1bf1e049afd710a7
Emilia Kura <emillia.kura@gmail.com>
2026-07-18 13:19:35 +0200

    feat: prepare fields & mrthods for post login endpoint

--- include/BackendClient.h
+++ include/BackendClient.h
@@ -8,7 +8,9 @@
 class BackendClient {
     static const char* BASE_URL;
     static const char* ACTIVATE_URL;
+    static const char* AUTH_URL;
 
 public:
     static bool activateDevice();
+    static bool authenticateDevice();
 };
