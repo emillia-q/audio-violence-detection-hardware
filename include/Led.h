ab95982bb4effd6b6ced2548316e0f2d000c23ce
Emilia Kura <emillia.kura@gmail.com>
2026-09-03 13:01:04 +0200

    feat: add Led class with enumeration of error codes

---
+++ include/Led.h
@@ -0,0 +1,17 @@
+# pragma once
+
+#include <Arduino.h>
+
+enum ErrorCode {
+    HARDWARE_ERROR,
+    WIFI_ERROR,
+    HTTP_ERROR
+};
+
+class Led
+{
+    int _ledPin;
+
+public:
+    Led(int ledPin);
+};
